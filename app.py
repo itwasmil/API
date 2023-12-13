9df85f02a0bb52b9b922a1f625fabf3d6a9bfa85
Mila Vujovic <milabvujovic@gmail.com>
2023-12-13 14:07:32 +0100

    Editing the api code

--- app.py
+++ app.py
@@ -7,8 +7,11 @@ import os
 from io import BytesIO
 #import matplotlib.pyplot as plt
 from flask import Flask, send_from_directory
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
