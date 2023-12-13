47f9b466e64c61c6a88455fe64d8849a772e2b30
Mila Vujovic <milabvujovic@gmail.com>
2023-12-13 12:56:49 +0100

    Applying some modifications

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
 import shap
-import os
 
 from io import BytesIO
 #import matplotlib.pyplot as plt
@@ -60,6 +59,4 @@ def predict():
     return jsonify(combined_data)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
     app.run(debug=True)
