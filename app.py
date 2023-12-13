ac129c623151db23f1451b12df4ae62dfa04cf9d
itwasmil <123497031+itwasmil@users.noreply.github.com>
2023-12-13 14:46:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,9 @@ import os
 from io import BytesIO
 #import matplotlib.pyplot as plt
 from flask import Flask, send_from_directory
-from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/')
 def home():
